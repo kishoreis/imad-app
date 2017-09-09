6a4d4b03b4bd379e7257c494539528e5dd47c69b
kishoreis <30802260+kishoreis@users.noreply.github.com>
2017-09-09 18:59:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(hiii);
+app.get('/kishore', function (req, res) {
+  res.send(hiii);
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
